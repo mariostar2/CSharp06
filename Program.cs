9b0f5efe0b0b7befa6dcf0a917668a49d2443a11
KIM <alskfl7777@naver.com>
2022-10-18 15:45:58 +0900

    열거형 완료

---
+++ Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+
+namespace CSharp06
+{
+    internal class Program
+    {
+
+
+        interface IEat
+        {
+            //먹는 다는 행위(함수)를 규격화 한다 
+            void Eat();
+        }
+        //인터페이스(interface)
+        // = 추상클래스와 유사하고 규격을 만들 때 사용한다.
+        //클래스를 내부에서는 사용가능하나
+        class Person : IEat
+        {
+            public void Eat()
+            {
+                Console.WriteLine("사람이 밥을 먹는다");
+            }
+        }
+
+        class Lion : IEat
+        {
+            public void Eat()
+            {
+                Console.WriteLine("사자이 고기를 먹는다");
+            }
+        }
+
+        class Camel : IEat
+        {
+            public void Eat()
+            {
+                Console.WriteLine("낙타가 풀을 먹는다");
+            }
+        }
+
+        class Cat :IEat
+        {
+            public void Eat()
+            {
+                Console.WriteLine("고양이가 먹이를 먹는다");
+            }
+
+        }
+         
+        
+        class Dog :IEat
+        {
+            public void Eat()
+            {
+                Console.WriteLine("강아지가 사료를 먹는다");
+            }
+        }
+
+        class Cow : IEat
+        {
+            public void Eat()
+            {
+                Console.WriteLine("소가 먹이를 먹는다 ");
+            }
+                
+        }
+            
+       
+        static void Main(string[] args)
+        {
+            Program2 program2 = new Program2();
+            program2.Start();
+            return;
+
+            Person person = new Person();       //Person 객체 인스턴스
+            Lion lion = new Lion();             //Lion 객체 인스턴스.
+            Camel camel = new Camel();          //Camel 객체 인스턴스.
+            Cat cat = new Cat();                //cat 객체 인스턴스 생성
+            Dog dog = new Dog();                //Dog 객채 인스턴스 생성
+            Cow cow = new Cow();                //Cow 객채 인스턴스 생성
+
+            person.Eat();
+            lion.Eat();
+            camel.Eat();
+            cat.Eat();
+            dog.Eat();
+            cow.Eat();
+
+
+            Console.WriteLine();
+            EatFunc(person);
+            EatFunc(lion);
+            EatFunc(camel);
+            EatFunc(cat);
+            EatFunc(cow);
+            
+
+            Console.WriteLine();
+            EatFuncInter(person);
+            EatFuncInter(lion);
+            EatFuncInter(camel);
+            EatFuncInter(cat);
+
+            Console.WriteLine();
+            EatFuncObj(person);
+            EatFuncObj(lion);
+            EatFuncObj(camel);
+            EatFuncObj(cat);
+        }
+
+        //인터페이스를 사용해 객체간의 결합도를 낮춤
+        static void EatFuncInter(object target)
+        {
+            if(target is IEat)
+            {
+                IEat eat = target as IEat;
+                eat.Eat();                                 
+            }
+            
+        }
+
+
+        //인터페이스를 사용한 '아규먼트 패싱' 을 사용안한경우
+        // 오버로딩을 통해 매서드를 3배 늘림
+        static void EatFunc(Person p)
+        {
+            p.Eat();
+        }
+        static void EatFunc(Lion lion)
+        {
+            lion.Eat();
+        }
+        static void EatFunc(Camel camel)
+        {
+            camel.Eat();
+        }
+
+        static void EatFunc(Cat cat)
+        {
+            cat.Eat();
+        }
+        static void EatFunc(Dog dog)
+        {
+            dog.Eat();
+        }
+
+        static void EatFunc(Cow cow)
+        {
+            cow.Eat();      
+        }
+
+
+        //Object를 이용하자
+        //오버로딩을 통해서는 클래스가 늘어남에 따라 함수의 양도 증가하기 때문에
+        //모든 자료형을 대입할 수 있는 클래스를 만들어야함
+        static void EatFuncObj(object target)
+        {
+            if(target is Person)
+            {
+                Person p = target as Person;
+                p.Eat();
+            }
+            else if(target is Lion)
+            {
+                Lion lion = target as Lion;
+                lion.Eat();
+            }   
+
+            else if(target is Camel)
+            {
+                Camel camel = target as Camel;
+                camel.Eat();
+            }             
+        }               
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic; // 일반화 타입 컬렉션
+using System.Collections; // 일반 타입 컬랙션
+
+
+namespace CSharp06
+{
+    class Box<T> : IEnumerable<T>
+    {
+        T[] array;
+        int index;
+
+        //외부 공개 Getter(프로퍼티)
+        //Count를 통해 값을 가져갈때 자신의 값이 아닌 index변수의 값을 리턴하라.
+        public int Count
+        {
+            //기본적으로 존재하는 get기능을 내가 구현했다
+            //index의 '값'은 사실상 가지고 있는 값의 개수와 동일하다
+            //따라 get(가져갈수만 있는 )변수를 만들고 index값을 반환시켰다
+            get
+            {
+                return index;
+            }
+            // int count 변수는 set기능을 구현하지 않아서 대입불가.
+        }
+
+        public Box()
+        {
+            array = new T[20];
+        }
+
+        //값을 대입 할수 있는 Add함수.
+        public void Add(T value)
+        {
+            array[index] = value;
+            index++;
+        }
+
+        //값을 대입 할수 있는 indexer.
+        public T this[int index]
+        {
+            get
+            {
+                return array [index];
+            }
+        }
+
+        //아래 함수의 이름은 GetEnumerator이다
+        public IEnumerator<T> GetEnumerator()
+        {
+            //횟수를 array.Length만큼 돌면 원하는 결과가 나오지 않는다.
+            //왜? 현재 배열은 20개의 길이를 가지지만 이것은 대입받는 값의 갯수는 아니다.
+            //따라서 실제 대입받은 값의 개수만큼 열거(=Enumerate)시킨다
+            for (int i = 0; i < Count; i++)
+                yield return array[i];
+        }
+        //일반 버전 열거자도 필요하다
+        //아래는 IEnumerable 내부에도 존재하는 GetEnumerator다
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+
+    internal class Program2
+    {
+        //Enumeratr : 수를 세다 (열거형)
+
+        //c#의 모든 컬렉션들은 아래의 두 인터페이스를 상속받아 구현하고 있다.
+        //IEnumerator : 데이터를 리턴하는 열거자;
+        //Ieumerable : 열거지를 리턴하는 Getter
+        public void Start()
+        {
+            if (false)
+            {
+                IEnumerator ie = GetEnumerator(); //열거자를 리턴한다.
+                Console.WriteLine($"현재 값은 :{ie.Current}");
+
+                ie.MoveNext();
+                Console.WriteLine($"현재 값은 : {ie.Current}");
+
+                ie.MoveNext();
+                Console.WriteLine($"현재 값은 : {ie.Current}");
+
+                ie.MoveNext();
+                Console.WriteLine($"현재 값은: {ie.Current}");
+            }
+
+            //for-each 구현하기
+
+            //이 시점에서 객체 box의 일반화 타입은 int라고 명시된다.
+            Box<int> box = new Box<int>();
+            foreach (int num in box)
+            {
+                Console.WriteLine(num);
+            }
+            // 이 시점에서 객체 box2의 일반화 타입은 string이라고 명시되어 있다;
+            Box<string> box2 = new Box<string>();
+            foreach (string str in box2)
+            {
+                Console.WriteLine(str);
+            }
+
+        }
+
+        //IEnumerable의 자동 완성형이다.
+        //열거자를 반환하는 함수는 MoveNext,Reset,Current를 자동으로 만든다.
+        IEnumerator GetEnumerator()
+        {
+
+            // MoveNext가 실행이 되면 다음 yleld 지점까지 실행한다
+            Console.WriteLine("1번");
+            yield return 1;             //yleld 대기 
+
+            Console.WriteLine("2번");
+            yield return 2;
+
+            Console.WriteLine("3번");
+        }
+    }
+}
